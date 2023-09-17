cc0abd5c831989f8c4b3e6308b6ba2a97f303a80
Sean Huynh <110818854+seangh125@users.noreply.github.com>
2023-09-18 02:26:21 +1000

    Update script.js
    Added in a Function to get date and time and added some extra tabs and titles.
    
    (Still need to link database data and create a function to call marks)

--- js/script.js
+++ js/script.js
@@ -2,24 +2,30 @@
 const resultsData = [
     {
       id: 1,
-      title: 'Presentation 1',
-      date: '2023-09-15',
-      presenter: 'John Doe',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      Subject: 'Algebra',
+      Date: getCurrentDate(),
+      Time: getCurrentTime(), 
+      Name: 'John Doe',
+      Mark: '85',
+      Description: 'Pull Generic Description based on students marks',
     },
     {
       id: 2,
-      title: 'Presentation 2',
-      date: '2023-09-20',
-      presenter: 'Jane Smith',
-      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      Subject: 'Geometry',
+      Date: getCurrentDate(),
+      Time: getCurrentTime(),
+      Name: 'Jane Smith',
+      Mark: '68',
+      Description: 'Pull Generic Description based on students marks',
     },
     {
       id: 3,
-      title: 'Presentation 3',
-      date: '2023-09-25',
-      presenter: 'Bob Johnson',
-      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      Subject: 'Calculus',
+      Date: getCurrentDate(),
+      Time: getCurrentTime(),
+      Name: 'Bob Johnson',
+      Mark: '59',
+      Description: 'Pull Generic Description based on students marks',
     },
     // Add more result objects as needed
   ];
@@ -37,14 +43,41 @@ const resultsData = [
       resultElement.classList.add('result');
   
       resultElement.innerHTML = `
-        <h2>${result.title}</h2>
-        <p><strong>Date:</strong> ${result.date}</p>
-        <p><strong>Presenter:</strong> ${result.presenter}</p>
-        <p><strong>Description:</strong> ${result.description}</p>
+        <h2>${result.Subject}</h2>
+        <p><strong>Date:</strong> ${result.Date}</p>
+        <p><strong>Time:</strong> ${result.Time}</p>
+        <p><strong>Presenter:</strong> ${result.Name}</p>
+        <p><strong>Mark:</strong> ${result.Mark}</p>
+        <p><strong>Description:</strong> ${result.Description}</p>
       `;
   
       resultsContainer.appendChild(resultElement);
     });
+  }
+  // Function to display current time.
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
+  // Function to get the current date.
+  function getCurrentDate(){
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1);
+    const day = now.getDate().toString();
+    return `${day}-${month}-${year}`;
+  }
+
+  //function to get the users mark.
+  function getMark(){
+
+  }
+
+  function redirectToFeedbackPage(){
+    window.location.href = 'feedbackPage.html';
   }
   
   // Call the displayResults function to populate the page with data
