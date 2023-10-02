dc05595fa3a2a40040a58b31b86fd2e02121bc01
Sean Huynh <110818854+seangh125@users.noreply.github.com>
2023-10-03 01:40:23 +1100

    Added a call for database in server.js

--- server.js
+++ server.js
@@ -46,3 +46,41 @@ app.use('/', userRoute);
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Quiz Schema
+const quizResultSchema = new mongoose.Schema({
+    user : String,
+    score: Number
+});
+
+const QuizResult = mongoose.model('QuizResult', quizResultSchema);
+
+//saving Quiz results.
+app.post('/api/quizresults', (req, res) => {
+    const {user, score} = req.body;
+
+    const quizResult = new QuizResult({
+        user, 
+        score
+    });
+
+    quizResult.save()
+        .then (savedResult => {
+            res.json(savedResult);
+        })
+        .catch(error => {
+            res.status(500).json({ error : 'Failed to save quiz result.'});
+        });
+});
+
+app.get('api/quizresults', (req, res) => {
+    QuizResult.find()
+        .then(results => {
+            res.json(results);
+        })
+        .catch(error => {
+            res.status(500).json({error: 'Failed to retrieve quiz results.'});
+        });
+});
+
+
