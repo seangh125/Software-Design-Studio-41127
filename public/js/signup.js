9758bbe50e25f968fefbbe5ab2153ecb07db0d45
13934447 <62012692+13934447@users.noreply.github.com>
2023-10-27 17:48:52 +1100

    Signup field validation and already used email validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("signupForm");
-    const submitButton = document.getElementById("submitButton");
-  
-    submitButton.addEventListener("click", function () {
+  const form = document.getElementById("signupForm");
+  const submitButton = document.getElementById("submitButton");
+  const emailError = document.getElementById("email-error");
+  const generalError = document.getElementById("general-error");
+
+  submitButton.addEventListener("click", function () {
+      emailError.textContent = "";
+      generalError.textContent = "";
+
       const formData = new FormData(form);
-      // Convert the FormData object to a plain JavaScript object
       const formDataObject = {};
       formData.forEach((value, key) => {
-        formDataObject[key] = value;
+          formDataObject[key] = value;
       });
-  
-      // Send the data as JSON
+
       fetch("/registerUser", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", 
-        },
-        body: JSON.stringify(formDataObject), 
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formDataObject),
       })
-        .then((response) => response.json())
-        .then((data) => {
-          // Handle the response data
-          console.log("Response:", data);
-        })
-        .catch((error) => {
+      .then((response) => response.json())
+      .then((data) => {
+          if (data.error) {
+              if (data.error === 'Email is already in use') {
+                  emailError.textContent = data.error;
+              } else if (data.error === 'All fields are required') {
+                  generalError.textContent = data.error;
+              }
+          } else {
+              window.location.href = '/';
+          }
+      })
+      .catch((error) => {
           console.error("Error:", error);
-        });
-    });
+      });
   });
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,17 @@ const User = require('../models/user');
 
 router.post('/registerUser', async (req, res) => {
   const formData = req.body;
+
+  if (!formData.firstname || !formData.surname || !formData.email || !formData.password || !formData.education) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  const existingUser = await User.findOne({ email: formData.email });
+  if (existingUser) {
+    return res.status(409).json({ error: 'Email is already in use' });
+  }
+
   try {
-    // Create a new user document
     const newUser = new User({
       firstname: formData.firstname,
       surname: formData.surname,
@@ -14,7 +23,6 @@ router.post('/registerUser', async (req, res) => {
       education: formData.education,
     });
 
-    // Save the user document to MongoDB
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
   } catch (err) {
