ce68adff0a76f357825392b9708229c97e633a3c
13934447 <62012692+13934447@users.noreply.github.com>
2023-11-02 19:35:20 +1100

    update user education after result

--- routes/resultRoute.js
+++ routes/resultRoute.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const Result = require('../models/resultModel'); 
+const User = require('../models/user');
 
 // Create a route to save the difficulty level
 router.post('/save-Result', async (req, res) => {
   try {
     const { currentDifficulty, sessionIdentifier } = req.body;
 
-    // Save the current difficulty level, current date, and session identifier (as a string) to MongoDB
+    const user = await User.findOne({ email: req.session.user });
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    user.education = currentDifficulty;
+
+    req.session.education = currentDifficulty;
+
+    await user.save();
+
+    // Save the current difficulty level, current date, and session identifier to MongoDB
     const result = new Result({
       difficulty: currentDifficulty,
       sessionIdentifier: sessionIdentifier,
